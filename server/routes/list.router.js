6099b446c0fcd5e85d0855364d63566b8da466f3
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-17 09:20:53 -0500

    database get for server:=

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,4 +4,27 @@ const router = express.Router();
 
 const pool = require("../modules/pools");
 
+router.get('/', (req,res) => {
+    console.log('inside GET');
+    let queryText = `SELECT * FROM "list"
+    ORDER BY "id"
+    ;`;
+    pool 
+    .query(queryText)
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((err) => {
+        console.log('ERROR in GET query', err);
+        res.sendStatus(500);
+    });
+});
+
+
+
+
+
+
+
+
 module.exports = router;
